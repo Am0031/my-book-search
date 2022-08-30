db2bbca85b49f38e889894b000e82b184f819493
Amelie Pira <amelie.pira@gmail.com>
2022-08-30 15:11:16 +0100

    added saveBook resolver

---
+++ server/resolvers/saveBook.js
@@ -0,0 +1,24 @@
+const { AuthenticationError } = require("apollo-server-express");
+
+const { User } = require("../models");
+
+const saveBook = async (_, { input }, { user }) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      { _id: user._id },
+      {
+        $push: {
+          savedBooks: input,
+        },
+      },
+      { new: true, runValidators: true }
+    );
+
+    return updatedUser;
+  } catch (err) {
+    console.log(err);
+    throw new AuthenticationError("Failed to save book");
+  }
+};
+
+module.exports = saveBook;
