667166d67afd9e4f8a010d1372000798dcfa71db
Amelie Pira <amelie.pira@gmail.com>
2022-08-30 15:30:04 +0100

    added mutations in mutations.js

---
+++ client/src/graphql/mutations.js
@@ -0,0 +1,67 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation Mutation($input: LoginInput!) {
+    login(input: $input) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const SIGNUP = gql`
+  mutation Mutation($input: SignupInput!) {
+    signup(input: $input) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation Mutation($input: SaveBookInput!) {
+    saveBook(input: $input) {
+      id
+      username
+      email
+      password
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation Mutation($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      id
+      username
+      email
+      password
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
